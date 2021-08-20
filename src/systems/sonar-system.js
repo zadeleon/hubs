8891f2e87d9ea9cdb66e754a47e6eef7c5dba165
John Shaughnessy <johnfshaughnessy@gmail.com>
2021-08-20 09:19:49 -0700

    Play a sound at a position
    If we enqueue a command to play a sound following an object3D at some
    point in the future, then we also have to check if that object3D is
    still in the scene at the time we want to play the sound.
    
    To avoid that complexity, we just save the positions of the avatars and
    play a sound at each position. This will result in the wrong sound
    effects when avatars are moving around or when they leave in between the
    keypress and the audio, but most of the time it's mostly correct, and
    that's good enough for a first version.

--- src/systems/sonar-system.js
+++ src/systems/sonar-system.js
@@ -3,18 +3,15 @@ import { SOUND_QUACK } from "./sound-effects-system";
 
 export function sonarSystemTick(userinput, sfx) {
   if (!userinput.get(paths.actions.pingSound)) return;
-  console.log(sfx.playPositionalSoundFollowing);
   const avatarEls = Array.from(document.querySelectorAll("[avatar-audio-source]"));
   const positions = avatarEls.map(avatarEl => {
-    //const position = new THREE.Vector3();
-    //avatarEl.object3D.getWorldPosition(position);
-    //return position;
-    return avatarEl.object3D;
+    const position = new THREE.Vector3();
+    avatarEl.object3D.getWorldPosition(position);
+    return position;
   });
-  console.log(positions);
   positions.forEach((position, index) => {
     setTimeout(() => {
-      sfx.playPositionalSoundFollowing(SOUND_QUACK, position, false);
+      sfx.playPositionalSoundAt(SOUND_QUACK, position, false);
     }, 300 * index);
   });
 }
